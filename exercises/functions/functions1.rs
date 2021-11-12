26e7063be271a56329de2d0caf2ec4adaa225d73
Jonny <jonnymthdev@gmail.com>
2021-11-12 11:06:24 +0100

    Ajout des exercices rustlings dans le git

---
+++ exercises/functions/functions1.rs
@@ -0,0 +1,10 @@
+// functions1.rs
+// Make me compile! Execute `rustlings hint functions1` for hints :)
+
+// I AM DONE
+fn main() {
+    call_me();
+}
+fn call_me(){
+    //il na rien ici
+}
---
+++ exercises/modules/modules1.rs
@@ -0,0 +1,15 @@
+// modules1.rs
+// Make me compile! Execute `rustlings hint modules1` for hints :)
+
+// I AM  DONE
+
+mod sausage_factory {
+    pub fn make_sausage() {
+        println!("sausage!");
+    }
+}
+
+
+fn main() {
+  sausage_factory::make_sausage;
+}
