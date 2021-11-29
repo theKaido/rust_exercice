0932d674f1f467e5ca5a4c653db04e75a565a673
Jonny <jonnymthdev@gmail.com>
2021-11-30 00:57:03 +0100

    Ajout des exo macros

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -1,14 +1,19 @@
 // macros2.rs
 // Make me compile! Execute `rustlings hint macros2` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
-fn main() {
-    my_macro!();
-}
+//macro_rules! my_macro; this function doesnt work se renseigner
+//sur les prototypes des fonctions en rust
 
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
 }
+
+
+fn main() {
+    my_macro!();
+}
+
