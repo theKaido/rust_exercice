b291607f0b5b5008db1eb1689ac581d12c7555c0
Jonny <jonnymthdev@gmail.com>
2021-11-30 01:07:33 +0100

    Exo clippy finit

---
+++ exercises/clippy/target/debug/deps/clippy2-3d6a964973b8be31.d
@@ -0,0 +1,7 @@
+/Users/mathanaruban/Desktop/rustlings/exercises/clippy/target/debug/deps/clippy2-3d6a964973b8be31.rmeta: clippy2.rs
+
+/Users/mathanaruban/Desktop/rustlings/exercises/clippy/target/debug/deps/clippy2-3d6a964973b8be31.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
